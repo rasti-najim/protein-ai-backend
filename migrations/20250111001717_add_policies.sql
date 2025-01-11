0a249dbdc02409e4584cf26bf3e15fc250705dcd
Rasti Aldawoodi <rastirasheed@gmail.com>
2025-01-10 20:07:44 -0500

    fix

--- migrations/20250111001717_add_policies.sql
+++ migrations/20250111001717_add_policies.sql
@@ -4,13 +4,13 @@ ALTER TABLE meals ENABLE ROW LEVEL SECURITY;
 
 -- Create policy for users table
 CREATE POLICY "Users can view their own profile"
-  ON "users "
+  ON "users"
   FOR SELECT
   TO authenticated
   USING (auth.uid() = id);
 
 CREATE POLICY "Users can insert their own profile"
-  ON "users "
+  ON "users"
   FOR INSERT
   TO authenticated
   USING (auth.uid() = id);
@@ -22,7 +22,7 @@ CREATE POLICY "Users can update their own profile"
   USING (auth.uid() = id);
 
 CREATE POLICY "Users can delete their own profile"
-  ON "users "
+  ON "users"
   FOR DELETE
   TO authenticated
   USING (auth.uid() = id);
