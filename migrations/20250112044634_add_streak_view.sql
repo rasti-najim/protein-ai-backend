a4d5748938dd19b20eb39f351b7a466a32c538f2
Rasti Aldawoodi <rastirasheed@gmail.com>
2025-01-11 23:48:49 -0500

    add streak view

---
+++ migrations/20250112044634_add_streak_view.sql
@@ -0,0 +1,22 @@
+ALTER TABLE streaks DROP COLUMN streak_level_id;
+
+CREATE VIEW user_streak_view AS
+SELECT
+  us.user_id,
+  us.current_streak,
+  us.max_streak,
+  (
+    SELECT sl.name
+    FROM streak_levels sl
+    WHERE sl.threshold <= us.current_streak
+    ORDER BY sl.threshold DESC
+    LIMIT 1
+  ) AS streak_name,
+  (
+    SELECT sl.emoji
+    FROM streak_levels sl
+    WHERE sl.threshold <= us.current_streak
+    ORDER BY sl.threshold DESC
+    LIMIT 1
+  ) AS streak_emoji
+FROM user_streaks us;
